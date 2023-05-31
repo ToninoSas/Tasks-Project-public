a72330e7f6270de92dd4786ca9bb2a5935a8e324
ToninoSas <antonioprencipe2004@gmail.com>
2023-05-31 19:44:04 +0200

    aggiunto file per le credenziali

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ from flask import Flask, redirect, render_template, request, session, url_for
 from admin_page import admin_page
 
 import db
+import json, os
 
 app = Flask(__name__, )
-app.secret_key = 'INSERT SECRET KEY'
 
-PROFILE_PSW = 'INSERT PASSWORD'
+if not os.path.isfile('credentials.json'):
+    print('Credenziali mancanti. Creare file credentials.json')
 
-app.config['session'] = session
-app.config['PROFILE_PSW'] = PROFILE_PSW
+with open('credentials.json', 'r') as file:
+    data = file.read()
+
+dataJson = json.loads(data)
+
+secret_key_session = dataJson['secret_key_session']
+app.secret_key = secret_key_session
+
+app.config['session'] = secret_key_session
+app.config['PROFILE_PSW'] = dataJson['PROFILE_PSW']
 
 app.register_blueprint(admin_page)
 
